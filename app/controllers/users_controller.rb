d7a36ef9591d7274f508692ed018b90d1a5017e8
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-09-21 00:04:23 -0700

    Add #create action to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
